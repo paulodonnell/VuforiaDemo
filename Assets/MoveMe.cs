f2638e831cda1883eb6c0f40229724efad4a2e7d
Paul O Donnell <podonne5@gmail.com>
2017-03-01 11:48:56 +0000

    vuforia demo

---
+++ Assets/MoveMe.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour 
+{
+	public bool moveLeft = false;
+	public float moveThreshold = 4f;
+	public float moveAmount = 0.01f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float posX = this.transform.position.x;
+
+		//Debug.Log("x " + posX);
+
+		if (moveLeft) 
+		{
+			posX -= moveAmount;
+
+			if(posX <= -moveThreshold)
+			{
+				posX = -moveThreshold;
+				moveLeft = false;
+			}
+		}
+		else
+		{
+			posX += moveAmount;
+
+			if(posX >= moveThreshold)
+			{
+				posX = moveThreshold;
+				moveLeft = true;
+			}
+		}
+
+		this.transform.position = new Vector3(posX, this.transform.position.y, this.transform.position.z);
+	}
+}
